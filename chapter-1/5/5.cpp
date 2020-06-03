680403913a7d62f7d8e0047b5880bdefde5705cf
Polypro <66321814+Polypro@users.noreply.github.com>
2020-06-03 14:54:20 -0400

    did excersie 3 4 5 6

---
+++ chapter-1/5/5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	int v1 = 0;
+	int v2 = 3;
+	// std::cout << "The sum of " << v1 << " and " << v2 << " is " << v1 + v2 << std::endl; //
+	cout << "the sum of";
+	cout << v1;
+	cout << "and";
+	cout << v2;
+	cout << "is";
+	cout << v1 + v2;
+	cout << endl;
+}
---
+++ chapter-1/6/6.cpp
@@ -0,0 +1,3 @@
+it is not legal because the 2nd and the 3rd line start
+with an operand and that operand only have the right-hand
+operand. To make it right, type std::cout 
